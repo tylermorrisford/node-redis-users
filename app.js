c5723597995dc63fc0a88052791ddeafee28ce23
Tyler Morris Ford <tylerford@gmail.com>
2020-07-10 15:18:55 -0600

    add update/delete

--- app.js
+++ app.js
@@ -24,11 +24,45 @@ app.set('view engine', 'handlebars');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 
+// override poset method
 app.use(methodOverride('_method'));
 
 // search
 app.get('/', function(req, res, next){
     res.render('searchusers');
+})
+
+// add user
+app.get('/user/add', function(req, res, next){
+    res.render('adduser')
+})
+
+// delete user
+app.delete('/user/delete/:id', function(req, res, next){
+    client.del(req.params.id);
+    res.redirect('/');
+})
+
+// process add user
+app.post('/user/add', function(req, res, next){
+    let id = req.body.id
+    let first_name = req.body.first_name
+    let last_name = req.body.last_name
+    let email = req.body.email
+    let phone = req.body.phone
+
+    client.hmset(id, [
+        'first_name', first_name,
+        'last_name', last_name,
+        'email', email,
+        'phone', phone
+    ], function(err, reply) {
+        if (err){
+            console.log(err)
+        } 
+        console.log(reply);
+        res.redirect('/');
+    })
 })
 
 app.post('/user/search', function(req, res, next){
